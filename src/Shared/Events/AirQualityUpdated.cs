368fb6015edbac41ea754c99c16e77e3a9565a45
BeLove <stas1408123@gmail.com>
2026-01-27 12:27:50 +0300

    Added integration events between processor and ingestor

---
+++ src/Shared/Events/AirQualityUpdated.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Events
+{
+    public record AirQualityUpdated
+    {
+        public string Name { get; set; }
+        public int Co2 { get; set; }
+        public int Pm25 { get; set; }
+        public int Humidity { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ src/Shared/Events/MotionUpdated.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Events
+{
+    public record MotionUpdated
+    {
+        public string Name { get; set; }
+        public bool MotionDetected { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
