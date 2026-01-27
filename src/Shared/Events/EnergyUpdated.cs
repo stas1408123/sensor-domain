b6f572d4f0b2c04b9b497c1262530932630a240b
stas1408123 <55632622+stas1408123@users.noreply.github.com>
2026-01-27 12:42:50 +0300

    Added integration events between processor and ingestor (#2)

---
+++ src/Shared/Events/EnergyUpdated.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Events
+{
+    public record EnergyUpdated
+    {
+        public string Name { get; set; }
+        public double Energy { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
