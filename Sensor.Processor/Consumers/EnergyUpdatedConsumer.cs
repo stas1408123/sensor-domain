fdbd572e750b2fcc1bb6c6791a77f7b4e775ef1b
stas1408123 <55632622+stas1408123@users.noreply.github.com>
2026-01-27 18:04:33 +0300

    Feature/add processor  (#4)
    * Added basic processor implementation
    
    * Moved rabbitConfig to Shared

---
+++ Sensor.Processor/Consumers/EnergyUpdatedConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using Shared.Events;
+
+namespace SensorProcessor.Consumers
+{
+    public class EnergyUpdatedConsumer : IConsumer<EnergyUpdated>
+    {
+        public Task Consume(ConsumeContext<EnergyUpdated> context)
+        {
+            Console.WriteLine(context.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sensor.Processor/Program.cs
@@ -0,0 +1,28 @@
+using MassTransit;
+using SensorProcessor.Consumers;
+using Shared.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var rabbitMqSettings = builder.Configuration.GetSection("RabbitMq").Get<RabbitMQSettings>();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<AirQualityUpdatedConsumer>();
+    x.AddConsumer<EnergyUpdatedConsumer>();
+    x.AddConsumer<MotionUpdatedConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(rabbitMqSettings.Host, rabbitMqSettings.VirtualHost, h =>
+        {
+            h.Username(rabbitMqSettings.Username);
+            h.Password(rabbitMqSettings.Password);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+var app = builder.Build();
+
+app.Run();
