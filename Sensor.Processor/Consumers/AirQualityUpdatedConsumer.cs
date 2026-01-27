505ee663055d6dfe28be4dd5119f4cfb596fe555
BeLove <stas1408123@gmail.com>
2026-01-27 18:02:06 +0300

    Added basic processor implementation

---
+++ Sensor.Processor/Consumers/AirQualityUpdatedConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using Shared.Events;
+
+namespace SensorProcessor.Consumers
+{
+    public class AirQualityUpdatedConsumer : IConsumer<AirQualityUpdated>
+    {
+        public Task Consume(ConsumeContext<AirQualityUpdated> context)
+        {
+            Console.WriteLine(context.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sensor.Processor/Consumers/MotionUpdatedConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Shared.Events;
+
+namespace SensorProcessor.Consumers
+{
+    public class MotionUpdatedConsumer : IConsumer<MotionUpdated>
+    {
+        public Task Consume(ConsumeContext<MotionUpdated> context)
+        {
+
+            Console.WriteLine(context.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
